4784b33b12f0a66be900f4db0e8bce8b7a4cb4c4
sathagesey33 <abdukadirmuhanad@gmail.com>
2023-07-26 14:09:32 -0700

    path

--- path_list.c
+++ path_list.c
@@ -1,33 +1,31 @@
 #include "main.h"
-#include <string.h>
-#include <stdlib.h>
 
-nodeptr listpath()
+nodeptr listpath(void)
 {
-    nodeptr head = malloc(sizeof(pathlist));
-    nodeptr temp = head;
-    char *token, *dir, *path;
+	nodeptr head = malloc(sizeof(pathlist));
+	nodeptr temp = head;
+	char *token, *dir, *path;
 
-    path = getenv("PATH");
-    dir = strdup(path);
-    if (dir != NULL)
-    {
-        token = strtok(dir, ":");
-        while (token != NULL)
-        {
-            head->exe_path = strdup(token);
-            if (head->exe_path == NULL)
-                return (NULL);
-            head->next = malloc(sizeof(pathlist));
-            if (head->next == NULL)
-                return (NULL);
-            head = head->next;
-            token = strtok(NULL, ":");
-        }
-        head->exe_path = NULL;
-        head->next = NULL;
-    }
-    head = temp;
-    free(dir);
-    return (head);
+	path = getenv("PATH");
+	dir = strdup(path);
+	if (dir != NULL)
+	{
+		token = strtok(dir, ":");
+	while (token != NULL)
+	{
+		head->exe_path = strdup(token);
+		if (head->exe_path == NULL)
+		return (NULL);
+		head->next = malloc(sizeof(pathlist));
+		if (head->next == NULL)
+		return (NULL);
+		head = head->next;
+		token = strtok(NULL, ":");
+	}
+		head->exe_path = NULL;
+		head->next = NULL;
+	}
+		head = temp;
+	free(dir);
+	return (head);
 }
