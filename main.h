75d14044ba6bb11f6f4f429b5a458bcc6750e2ce
Mburu-Elvis <mburu.elvis1@students.jkuat.ac.ke>
2023-07-26 01:22:08 -0700

    task 1 error checking

--- main.h
+++ main.h
@@ -1,4 +1,12 @@
 #ifndef _MAIN_H
 #define _MAIN_H
 
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+extern char **environ;
+
 #endif
--- simple_shell.c
+++ simple_shell.c
@@ -3,11 +3,46 @@
 
 /**
  * main - the main program of the shell
+ * @argc: number of arguments
+ * @argv: array of strings of arguments passed
  *
  * Return: 0 on SUCCESS, else status
  */
-int main(void)
+int main(int argc, char **argv)
 {
-	printf("Hello, shell\n");
+	size_t n = 0;
+	int i = 0, j = 0;
+	char *buf = malloc(sizeof(char) * 1024), *token = NULL;
+	char *delims = " \n", **av;
+	pid_t child = 1;
+
+	while (1)
+	{
+		if (isatty(STDIN_FILENO))
+			write(1, "#cisfun$ ", 9);
+		j = getline(&buf, &n, stdin);
+		if (j == -1)
+			return (0);
+		av = malloc(sizeof(char) * 1024);
+		token = strtok(buf, delims);
+		av[i] = strdup(token);
+		i++;
+		av[i] = NULL;
+		i = 0;
+		child = fork();
+		if (child == 0)
+		{
+			if (execve(av[0], av, environ) == -1)
+				perror(argv[0]);
+			while (av[i] != NULL)
+			{
+				free(av[i]);
+				i++;
+			}
+		}
+		else
+			wait(NULL);
+	}
+	free(buf);
 	return (0);
 }
