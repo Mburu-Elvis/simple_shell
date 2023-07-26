d235beb339ffa6aa8bacffb773da709e64a83128
Mburu-Elvis <mburu.elvis1@students.jkuat.ac.ke>
2023-07-26 03:16:04 -0700

    mem check

--- simple_shell.c
+++ simple_shell.c
@@ -8,41 +8,47 @@
  *
  * Return: 0 on SUCCESS, else status
  */
-int main(int argc, char **argv)
+int main(void)
 {
 	size_t n = 0;
-	int i = 0, j = 0;
-	char *buf = malloc(sizeof(char) * 1024), *token = NULL;
+	int i = 0, j = 0, k;
+	char *buf = NULL, *token;
 	char *delims = " \n", **av;
 	pid_t child = 1;
 
-	(void)argc;
 	while (1)
 	{
 		if (isatty(STDIN_FILENO))
 			write(1, "#cisfun$ ", 9);
 		j = getline(&buf, &n, stdin);
 		if (j == -1)
-			return (0);
-		av = malloc(sizeof(char) * 1024);
+		{
+			free(buf);
+			break;
+		}
+		av = malloc(sizeof(char *) * 1024);
 		token = strtok(buf, delims);
-		av[i] = strdup(token);
-		i++;
+		while (token != NULL)
+		{
+			av[i] = strdup(token);
+			token = strtok(NULL, delims);
+			i++;
+		}
 		av[i] = NULL;
 		i = 0;
 		child = fork();
 		if (child == 0)
 		{
 			if (execve(av[0], av, environ) == -1)
-				perror(argv[0]);
-			while (av[i] != NULL)
-			{
-				free(av[i]);
-				i++;
-			}
+				perror("./hsh");
 		}
 		else
+		{
 			wait(NULL);
+		}
+		for (k = 0; av[k] != NULL; k++)
+			free(av[i]);
+		free(av);
 	}
 	free(buf);
 	return (0);
