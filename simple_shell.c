d6922f4c9124d8870f13f7df7d71542e37742ac9
Mburu-Elvis <mburu.elvis1@students.jkuat.ac.ke>
2023-07-25 23:41:09 -0700

    task 0

---
+++ simple_shell.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - the main program of the shell
+ *
+ * Return: 0 on SUCCESS, else status
+ */
+int main(void)
+{
+	printf("Hello, shell\n");
+	return (0);
+}
