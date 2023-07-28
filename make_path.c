a3ad8d8594fa4f46546e0123e21aad6c185604da
Mburu-Elvis <mburu.elvis1@students.jkuat.ac.ke>
2023-07-28 07:24:23 -0700

    task 1 betty

--- make_path.c
+++ make_path.c
@@ -8,27 +8,26 @@
  */
 char *make_path(char *str)
 {
-        nodeptr head, temp;
+	nodeptr head, temp;
 	char *abs_path;
 
-        head = listpath();
-        temp = head;
-
-        while (head->next != NULL && head->exe_path != NULL)
-        {
-                abs_path = malloc(strlen(head->exe_path) + strlen(str) + 2);
-                strcpy(abs_path, head->exe_path);
-                strcat(abs_path, "/");
-                strcat(abs_path, str);
-                if (access(abs_path, F_OK) == 0)
-                {
-                        head = temp;
-                        free_list(head);
-                        return (abs_path);
-                }
-                head = head->next;
-                free(abs_path);
-        }
+	head = listpath();
+	temp = head;
+	while (head->next != NULL && head->exe_path != NULL)
+	{
+		abs_path = malloc(strlen(head->exe_path) + strlen(str) + 2);
+		strcpy(abs_path, head->exe_path);
+		strcat(abs_path, "/");
+		strcat(abs_path, str);
+		if (access(abs_path, F_OK) == 0)
+		{
+			head = temp;
+			free_list(head);
+			return (abs_path);
+		}
+		head = head->next;
+		free(abs_path);
+	}
 	head = temp;
 	free_list(head);
 	return (str);
